18e393a1499e67e1275184b0d5c9d08dbe5e0bfd
Archit404Error <4architmehta@gmail.com>
2022-06-04 20:24:35 -0400

    Updated friend updates

--- Profiles/userProfile.js
+++ Profiles/userProfile.js
@@ -1,44 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import DisplayProfile from "./displayProfile";
 import Context from '../Context/context';
 
-export default class extends React.Component {
+export default ({ navigation }) => {
 
-    static contextType = Context;
+    const context = useContext(Context);
 
-    state = {
-        id: -1,
-        name: 'Sample Name',
-        profPic: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png',
-        friends: [],
-        events: [],
-    }
-
-    constructor(props) {
-        super(props);
-    }
-
-    componentDidMount() {
-        this.state.id = this.context.id;
-        this.state.name = this.context.fullName;
-        this.state.events = this.context.acceptedEvents;
-        this.state.friends = this.context.friends;
-        this.state.profPic = this.context.profilePic;
-        this.setState(this.state);
-    }
-
-    render() {
-        return (
-            <DisplayProfile
-                id={this.state.id}
-                name={this.state.name}
-                events={this.state.events}
-                friends={this.state.friends}
-                profilePic={this.state.profPic}
-                navigation={this.props.navigation}
-                editable={true}
-            />
-        );
-    }
+    return (
+        <DisplayProfile
+            id={context.id}
+            name={context.fullName}
+            events={context.acceptedEvents}
+            friends={context.friends}
+            profilePic={context.profilePic}
+            navigation={navigation}
+            editable={true}
+        />
+    );
 }
