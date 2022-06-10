0a2816fc76111147a14ba4ce66bc547c108074d6
Archit404Error <4architmehta@gmail.com>
2022-06-09 20:37:48 -0400

    Make stories timed again

--- Friends/story.js
+++ Friends/story.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { TouchableOpacity, Image, Modal, View, StatusBar } from "react-native";
+import { TouchableOpacity, Image, Modal, View, StatusBar, Text } from "react-native";
 import { EvilIcons } from '@expo/vector-icons'
 
 import { styles } from "../styles";
@@ -9,6 +9,9 @@ export default ({ id, forUpload, image }) => {
     const [previewPic, setPreview] = useState("https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png")
     const [visible, setVisible] = useState(false)
     const [viewed, setViewed] = useState(false);
+    const [viewDuration, setDuration] = useState(10);
+    const [intervalId, setId] = useState(-1);
+    var storyLen = 10;
 
     useEffect(() => {
         (async () => {
@@ -16,6 +19,24 @@ export default ({ id, forUpload, image }) => {
             setPreview((await json).profilePic)
         })()
     }, [])
+
+    useEffect(() => {
+        if (visible) {
+            setId(setInterval(() => { console.log(storyLen); setDuration(storyLen--) }, 1000))
+        }
+    }, [visible])
+
+    const closeStory = () => {
+        clearInterval(intervalId)
+        setVisible(false)
+        setViewed(true)
+        storyLen = 10
+        setDuration(storyLen)
+    }
+
+    useEffect(() => {
+        if (viewDuration <= 0) closeStory()
+    }, [viewDuration])
 
 
     return (
@@ -33,10 +54,11 @@ export default ({ id, forUpload, image }) => {
             >
                 <StatusBar hidden />
                 <View style={{ alignItems: 'flex-end', marginRight: 10, marginTop: 10 }}>
-                    <TouchableOpacity onPress={() => { setViewed(true); setVisible(false) }}>
+                    <TouchableOpacity onPress={closeStory}>
                         <EvilIcons name="close" size={30} color="black" />
                     </TouchableOpacity>
                 </View>
+                <Text style={styles.boldSubHeader}>{viewDuration}</Text>
                 <Image source={{ uri: image }} style={styles.storyContent} />
             </Modal>
         </>
